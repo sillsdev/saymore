d28fa3dc202bb362079fc1a7c2cc70162a294618
David Olson <domferrari@users.noreply.github.com>
2010-04-27 14:49:34 -0400

    Added Load/Save to ComponentFile class.

--- src/Sponge2/Model/ComponentFile.cs
+++ src/Sponge2/Model/ComponentFile.cs
@@ -1,29 +1,102 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Xml.Serialization;
+using Palaso.Reporting;
+using SilUtils;
 
 namespace Sponge2.Model
 {
+	/// ----------------------------------------------------------------------------------------
 	/// <summary>
-	/// Both sessions and people are made up of a number of files: an xml file we help them edit,
-	/// plus any number of other files (videos, texts, images, etc.).  Each of these is represented
-	/// by an object of this class.
+	/// Both sessions and people are made up of a number of files: an xml file we help them edit
+	/// (i.e. .session or .person), plus any number of other files (videos, texts, images, etc.).
+	/// Each of these is represented by an object of this class.
 	/// </summary>
+	/// ----------------------------------------------------------------------------------------
+	[XmlType("componentFile")]
 	public class ComponentFile
 	{
 		//autofac uses this, so that callers only need to know the path, not all the dependencies
 		public delegate ComponentFile Factory(string path);
 
+		// John: is it necessary to lug around the whole list? Do you see a problem setting
+		// the component file's type during contstruction and not every time it's requested?
 		private readonly IEnumerable<FileType> _fileTypes;
-		public string Path { get; set; }
 
+		[XmlIgnore]
+		public string Path { get; private set; }
+
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// Loads into a ComponentFile object the information from a standoff markup file
+		/// associated with the specified path.
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		public static ComponentFile Load(string path)
+		{
+			var standoffFile = GetStandoffFile(path);
+
+			// Future: Perform migration transforms here on standoffFilie when needed.
+
+			Exception e = null;
+
+			ComponentFile componentFile = (!File.Exists(standoffFile) ? new ComponentFile() :
+				XmlSerializationHelper.DeserializeFromFile<ComponentFile>(standoffFile, out e));
+
+			// Review: should this be fatal?
+			if (e != null)
+				ErrorReport.ReportFatalException(e);
+
+			// Review: will DI provide the file types later?
+			componentFile.Path = path;
+			return componentFile;
+		}
+
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// Constructs the full path to the (stand-off markup) data file for the session file.
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		private static string GetStandoffFile(string fileName)
+		{
+			return System.IO.Path.ChangeExtension(fileName, "smd");
+		}
+
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// Used mainly for serialization.
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		public ComponentFile()
+		{
+		}
+
+		/// ------------------------------------------------------------------------------------
 		public ComponentFile(string path, IEnumerable<FileType> fileTypes)
 		{
 			_fileTypes = fileTypes;
 			Path = path;
 		}
 
+		/// ------------------------------------------------------------------------------------
+		public bool Save()
+		{
+			var standoffFile = GetStandoffFile(Path);
+			Exception e = null;
+			bool result = XmlSerializationHelper.SerializeToFile(standoffFile, this, out e);
 
+			if (e != null)
+				ErrorReport.ReportNonFatalException(e);
+
+			return result;
+		}
+
+		/// ------------------------------------------------------------------------------------
+		/// John: do you have a philosophy behind making this a method rather than a property?
+		/// Or, is it arbitrary?
+		///
 		public FileType GetFileType()
 		{
 			var fileType = _fileTypes.FirstOrDefault(t => t.IsMatch(Path));
@@ -48,7 +121,7 @@ namespace Sponge2.Model
 		}
 
 		/// <summary>
-		/// The metadata do we have associated with this file.
+		/// The metadata we have associated with this file.
 		/// </summary>
 		public List<FieldValue> MetaDataValues
 		{
@@ -56,10 +129,10 @@ namespace Sponge2.Model
 		}
 #endif
 
+		/// ------------------------------------------------------------------------------------
 		public static ComponentFile CreateMinimalComponentFileForTests(string path)
 		{
 			return new ComponentFile(path, new FileType[]{});
 		}
 	}
-
 }
--- src/Sponge2/Model/FileType.cs
+++ src/Sponge2/Model/FileType.cs
@@ -5,42 +5,45 @@ namespace Sponge2.Model
 {
 	/// <summary>
 	/// Each file corresponds to a single kind of fileType.  The FileType then tells
-	/// us what controls are available for marking up, editting, or viewing that file.
+	/// us what controls are available for marking up, editing, or viewing that file.
 	/// </summary>
 	public class FileType
 	{
 		private readonly Func<string, bool> _isMatchPredicate;
 
+		public string Name { get; private set; }
+
+		/// ------------------------------------------------------------------------------------
 		public static FileType Create(string name, string matchForEndOfFileName)
 		{
-			return new FileType(name, p=>p.EndsWith(matchForEndOfFileName));
+			return new FileType(name, p=> p.EndsWith(matchForEndOfFileName));
 		}
 
+		/// ------------------------------------------------------------------------------------
 		public static FileType Create(string name, string[] matchesForEndOfFileName)
 		{
 			return new FileType(name, p => matchesForEndOfFileName.Any(ext => p.EndsWith(ext)));
 		}
 
+		/// ------------------------------------------------------------------------------------
 		public FileType(string name, Func<string, bool>isMatchPredicate)
 		{
 			Name = name;
 			_isMatchPredicate = isMatchPredicate;
 		}
 
-
-		public string Name { get; private set; }
-
+		/// ------------------------------------------------------------------------------------
 		public bool IsMatch(string path)
 		{
 			return _isMatchPredicate(path);
 		}
 	}
 
-
+	/// ------------------------------------------------------------------------------------
 	public class UnknownFileType : FileType
 	{
 		public UnknownFileType()
-			: base("Unknown", (path) => true)
+			: base("Unknown", path => true)
 		{
 		}
 	}

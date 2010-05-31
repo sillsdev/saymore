f1d5f4558af8176c24ebc78c605eab00e61a7642
John Hatton <hattonjohn@gmail.com>
2010-05-31 17:39:04 +1000

    Add beginnings of CSV export

---
+++ src/SayMore/ExportCommand.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SayMore.Model;
+using SayMore.Model.Fields;
+
+namespace SayMore
+{
+	/// <summary>
+	/// Exports metadata to file (currently, only csv)
+	/// </summary>
+	public class ExportCommand :Command
+	{
+		private readonly IEnumerable<ProjectElement> _elements;
+		private char _delimeter=',';
+
+		public ExportCommand(ElementRepository<Session> sessions)
+			: base("export")
+		{
+			_elements = sessions.AllItems.ToArray();
+		}
+
+		/// <summary>
+		/// for tests only
+		/// </summary>
+		public ExportCommand()
+			: base("export")
+		{
+		}
+
+		public override void Execute()
+		{
+			DoExport(_elements.ToArray());
+		}
+
+		public void DoExport(IEnumerable<ProjectElement> elements)
+		{
+			using(var dlg = new SaveFileDialog())
+			{
+				dlg.RestoreDirectory = true;
+				dlg.Title = "Export Session Data";
+				dlg.AddExtension = true;
+				dlg.AutoUpgradeEnabled = true;
+				dlg.Filter = "CSV (Comma delimited) (*.csv)|*.csv";
+				if(DialogResult.OK!= dlg.ShowDialog())
+				{
+					return;
+				}
+				DoExport(elements, dlg.FileName);
+			}
+		}
+
+		private void DoExport(IEnumerable<ProjectElement> elements, string path)
+		{
+			try
+			{
+				File.WriteAllText(path, GetFileString(elements));
+			}
+			catch(Exception e)
+			{
+				Palaso.Reporting.ErrorReport.NotifyUserOfProblem(e, "Something went wrong with the export.");
+			}
+		}
+
+		public string GetFileString(IEnumerable<ProjectElement> elements)
+		{
+			return GetFileString(
+				elements.Select(element => element.MetaDataFile.MetaDataFieldValues)
+					.Cast<IEnumerable<FieldValue>>().ToList());
+		}
+
+		public string GetFileString(IEnumerable<IEnumerable<FieldValue>> setsOfFields)
+		{
+			var builder = new StringBuilder();
+			IEnumerable<string> keys = GetKeys(setsOfFields);
+
+			builder.AppendLine(GetHeader(keys));
+			foreach (var fields in setsOfFields)
+			{
+				builder.AppendLine(GetValueLine(keys, fields));
+			}
+			return builder.ToString();
+		}
+
+		/* This is really embarrasing. Someone with linq skills should rewrite it */
+		private IEnumerable<string> GetKeys(IEnumerable<IEnumerable<FieldValue>> setsOfFields)
+		{
+			var lists = from e in setsOfFields
+						select e.Select(z=> z.FieldDefinitionKey);
+
+			var allKeys = new List<string>();
+			foreach (var list in lists)
+			{
+				allKeys.AddRange(list);
+			}
+			return allKeys.Distinct();
+		}
+
+		private string GetHeader(IEnumerable<string> keys)
+		{
+			var builder = new StringBuilder();
+
+			foreach (string key in keys)
+			{
+				builder.Append(EscapeIfNeeded(key) + _delimeter);
+			}
+			return builder.ToString().TrimEnd(_delimeter);
+		}
+
+		private string GetValueLine(IEnumerable<string> keys, IEnumerable<FieldValue> fields)
+		{
+			var builder = new StringBuilder();
+
+			foreach(string key in keys)
+			{
+				var f= fields.FirstOrDefault(x=> x.FieldDefinitionKey == key);
+				if (f == null)
+				{
+					builder.Append(string.Empty + _delimeter);
+				}
+				else
+				{
+					builder.Append(EscapeIfNeeded(f.Value) + _delimeter);
+				}
+			}
+			return builder.ToString().TrimEnd(_delimeter);
+		}
+
+		private string EscapeIfNeeded(string value)
+		{
+			if(value.Contains(_delimeter))
+			{
+				return '"' + value + '"';
+			}
+			return value;
+		}
+
+	}
+}
---
+++ src/SayMoreTests/ExportTests.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using SayMore;
+using SayMore.Model;
+using SayMore.Model.Fields;
+
+namespace Sponge2Tests
+{
+	public class ExportTests
+	{
+		[Test]
+		public void GetFileString_EmptyList_DoesntCrash()
+		{
+			var exporter = new ExportCommand();
+			exporter.GetFileString(new List<Session>().ToArray());
+		}
+
+		[Test]
+		public void GetFileString_TwoItemsWithDifferentFields_HeaderCombinesFields()
+		{
+			var exporter = new ExportCommand();
+			var s1 = new List<FieldValue>(new[]{
+				new FieldValue("one","string","uno"),
+				new FieldValue("two", "string", "dos")});
+			var s2 = new List<FieldValue>(new[]
+											{
+												new FieldValue("two", "string", "dos"),
+												new FieldValue("three", "string", "tres")
+											});
+			var result = exporter.GetFileString(new[] { s1, s2 });
+			Assert.AreEqual("one,two,three", result.Split('\n').First().TrimEnd());
+		}
+
+		[Test]
+		public void GetFileString_TwoItemsWithDifferentFields_CorrectValueLines()
+		{
+			var exporter = new ExportCommand();
+			var s1 = new List<FieldValue>(new[]{
+				new FieldValue("one","string","uno"),
+				new FieldValue("two", "string", "dos")});
+			var s2 = new List<FieldValue>(new[]
+											{
+												new FieldValue("two", "string", "dos"),
+												new FieldValue("three", "string", "tres")
+											});
+			var result = exporter.GetFileString(new[] { s1, s2 });
+			var lines = result.Split('\n').Select(l=>l.TrimEnd()).ToArray();
+			Assert.AreEqual("uno,dos", lines[1]);
+			Assert.AreEqual(",dos,tres", lines[2]);
+		}
+
+		[Test]
+		public void GetFileString_KeysAndFieldsHaveCommas_ProperlyQuoted()
+		{
+			var exporter = new ExportCommand();
+			var s1 = new List<FieldValue>(new[]{
+				new FieldValue("a,b","string","1,2")});
+			var result = exporter.GetFileString(new[] { s1});
+			var lines = result.Split('\n').Select(l => l.TrimEnd()).ToArray();
+			Assert.AreEqual("\"a,b\"", lines[0]);
+			Assert.AreEqual("\"1,2\"", lines[1]);
+		}
+	}
+}

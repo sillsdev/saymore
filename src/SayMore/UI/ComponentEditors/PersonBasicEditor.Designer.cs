65eddf903a5cede695988aa379a10b9c5e2eec3b
John Hatton <hattonjohn@gmail.com>
2010-06-23 18:02:59 +1000

    Set some more autoComplete keys.

--- src/SayMore/UI/ComponentEditors/PersonBasicEditor.Designer.cs
+++ src/SayMore/UI/ComponentEditors/PersonBasicEditor.Designer.cs
@@ -510,7 +510,7 @@ namespace SayMore.UI.ComponentEditors
 			// 
 			// _primaryLanguageLearnedIn
 			// 
-			this._languageAutoCompleteHelper.SetAutoCompleteKey(this._primaryLanguageLearnedIn, "");
+			this._languageAutoCompleteHelper.SetAutoCompleteKey(this._primaryLanguageLearnedIn, "languageLearnedIn");
 			this._primaryLanguageLearnedIn.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
 			this._primaryLanguageLearnedIn.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
 			this._tblLayoutPrimaryLanguage.SetColumnSpan(this._primaryLanguageLearnedIn, 3);
@@ -552,7 +552,7 @@ namespace SayMore.UI.ComponentEditors
 			// 
 			this._education.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
 						| System.Windows.Forms.AnchorStyles.Right)));
-			this._languageAutoCompleteHelper.SetAutoCompleteKey(this._education, "language");
+			this._languageAutoCompleteHelper.SetAutoCompleteKey(this._education, "education");
 			this._education.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
 			this._education.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
 			this._tblLayoutOuter.SetColumnSpan(this._education, 2);
@@ -581,7 +581,7 @@ namespace SayMore.UI.ComponentEditors
 			// 
 			this._primaryOccupation.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
 						| System.Windows.Forms.AnchorStyles.Right)));
-			this._languageAutoCompleteHelper.SetAutoCompleteKey(this._primaryOccupation, "");
+			this._languageAutoCompleteHelper.SetAutoCompleteKey(this._primaryOccupation, "occupation");
 			this._primaryOccupation.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
 			this._primaryOccupation.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
 			this._tblLayoutOuter.SetColumnSpan(this._primaryOccupation, 2);

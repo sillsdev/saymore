b970f41c29a211c73ce2c3609aa858dfe4d1659b
John Hatton <hattonjohn@gmail.com>
2010-07-01 14:34:10 +1000

    New from files--> New from device

--- src/SayMore/UI/NewSessionsFromFiles/NewSessionsFromFilesDlg.Designer.cs
+++ src/SayMore/UI/NewSessionsFromFiles/NewSessionsFromFilesDlg.Designer.cs
@@ -271,7 +271,7 @@ namespace SayMore.UI.NewSessionsFromFiles
 			this.ShowIcon = false;
 			this.ShowInTaskbar = false;
 			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-			this.Text = "New Sessions From Files";
+			this.Text = "New Sessions From Device";
 			this._mediaPlayerPanel.ResumeLayout(false);
 			this._panelProgress.ResumeLayout(false);
 			this._outerTableLayout.ResumeLayout(false);

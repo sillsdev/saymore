3ecd517dacdb526938d658cf52d4d9317b0df8fa
John Hatton <hattonjohn@gmail.com>
2011-04-01 07:55:02 +1000

    tweak test installer build

--- build/TestInstallerBuild.bat
+++ build/TestInstallerBuild.bat
@@ -2,6 +2,6 @@ call "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 
 pushd .
 copy "..\..\SaymoreDocumentation\SayMore.chm" ..\DistFiles
-MSbuild /target:installer /property:teamcity_build_checkoutDir=..\ /verbosity:detailed /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="*.*.6.789" /property:Minor="1"
+MSbuild /target:installer /property:teamcity_build_checkoutDir=..\ /verbosity:detailed /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="*.*.6.789" /property:Configuration=Release /property:Minor="1"
 popd
 PAUSE

d6a215b48786f1690d367926036effc02cb46fe2
David Olson <domferrari@users.noreply.github.com>
2011-06-30 09:16:59 -0400

    Added a tier and segment type that keeps track only of media start/stop positions. The media filename is at the tier level.

---
+++ src/SayMore/Transcription/Model/TimeOrderSegment.cs
@@ -0,0 +1,32 @@
+
+namespace SayMore.Transcription.Model
+{
+	public class TimeOrderSegment : SegmentBase, ITimeOrderSegment
+	{
+		public float Start { get; private set; }
+		public float Stop { get; private set; }
+
+		/// ------------------------------------------------------------------------------------
+		public TimeOrderSegment(ITier tier, float start, float stop) : base(tier)
+		{
+			Start = start;
+			Stop = stop;
+		}
+
+		/// ------------------------------------------------------------------------------------
+		/// <summary>
+		/// Returns the length of the segment in seconds.
+		/// </summary>
+		/// ------------------------------------------------------------------------------------
+		public float GetLength()
+		{
+			return (float)((decimal)Stop - (decimal)Start);
+		}
+
+		/// ------------------------------------------------------------------------------------
+		public override string ToString()
+		{
+			return string.Format("Start={0};  Length={1}", Start, Stop);
+		}
+	}
+}
---
+++ src/SayMore/Transcription/Model/TimeOrderTier.cs
@@ -0,0 +1,30 @@
+using SayMore.Transcription.UI;
+
+namespace SayMore.Transcription.Model
+{
+	public class TimeOrderTier : TierBase
+	{
+		public string MediaFileName { get; protected set; }
+
+		/// ------------------------------------------------------------------------------------
+		public TimeOrderTier(string filename) : this("Original", filename)
+		{
+		}
+
+		/// ------------------------------------------------------------------------------------
+		public TimeOrderTier(string displayName, string filename) : base(displayName)
+		{
+			DataType = TierType.TimeOrder;
+			MediaFileName = filename;
+			GridColumn = new AudioWaveFormColumn(this);
+		}
+
+		/// ------------------------------------------------------------------------------------
+		public TimeOrderSegment AddSegment(float start, float stop)
+		{
+			var segment = new TimeOrderSegment(this, start, stop);
+			_segments.Add(segment);
+			return segment;
+		}
+	}
+}

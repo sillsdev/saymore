7067eedd17634810bcac9ea654c19380d5105d37
David Olson <domferrari@users.noreply.github.com>
2011-08-17 14:55:54 -0400

    Fixed AudioVideoDataGatherer so it doesn't collect data on oral annotation audio files.

--- src/SayMore/Model/Files/DataGathering/AudioVideoDataGatherer.cs
+++ src/SayMore/Model/Files/DataGathering/AudioVideoDataGatherer.cs
@@ -1,8 +1,11 @@
 using System.Collections.Generic;
 using System.Linq;
+using SayMore.Properties;
+using SayMore.Transcription.UI;
 
 namespace SayMore.Model.Files.DataGathering
 {
+	/// ------------------------------------------------------------------------------------
 	public interface IProvideAudioVideoFileStatistics
 	{
 		MediaFileInfo GetFileData(string filePath);
@@ -11,10 +14,25 @@ namespace SayMore.Model.Files.DataGathering
 	public class AudioVideoDataGatherer :
 		BackgroundFileProcessor<MediaFileInfo>, IProvideAudioVideoFileStatistics
 	{
+		/// ------------------------------------------------------------------------------------
 		public AudioVideoDataGatherer(string rootDirectoryPath, IEnumerable<FileType> allFileTypes) :
 			base(rootDirectoryPath, allFileTypes.Where(t => t.IsAudioOrVideo),
 				path => new MediaFileInfo(path))
 		{
+		}
+
+		/// ------------------------------------------------------------------------------------
+		protected override void CollectDataForFile(string path)
+		{
+			// Don't collect info. on oral annotation files.
+			if (path.EndsWith(Settings.Default.OralAnnotationGeneratedFileAffix) ||
+				path.EndsWith("_" + OralAnnotationType.Translation) ||
+				path.EndsWith("_" + OralAnnotationType.Careful))
+			{
+				return;
+			}
+
+			base.CollectDataForFile(path);
 		}
 	}
 }

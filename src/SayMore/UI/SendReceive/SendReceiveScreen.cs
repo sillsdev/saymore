1f9511b1b99621547a54a7221e096127c9e39a6a
David Olson <domferrari@users.noreply.github.com>
2011-10-01 21:15:26 -0400

    Commented out for now.

--- src/SayMore/UI/SendReceive/SendReceiveScreen.cs
+++ src/SayMore/UI/SendReceive/SendReceiveScreen.cs
@@ -6,49 +6,49 @@ using SayMore.UI.ProjectWindow;
 namespace SayMore.UI.SendReceive
 {
 	/// ----------------------------------------------------------------------------------------
-	public class SendReceiveScreen : UserControl, ISayMoreView
-	{
-		/// ------------------------------------------------------------------------------------
-		public override string Text
-		{
-			get { return "Send/Receive"; }
-			set { }
-		}
+	//public class SendReceiveScreen : UserControl, ISayMoreView
+	//{
+	//    /// ------------------------------------------------------------------------------------
+	//    public override string Text
+	//    {
+	//        get { return "Send/Receive"; }
+	//        set { }
+	//    }
 
-		#region ISayMoreView Members
-		/// ------------------------------------------------------------------------------------
-		public void ViewActivated(bool firstTime)
-		{
-		}
+	//    #region ISayMoreView Members
+	//    /// ------------------------------------------------------------------------------------
+	//    public void ViewActivated(bool firstTime)
+	//    {
+	//    }
 
-		/// ------------------------------------------------------------------------------------
-		public void ViewDeactivated()
-		{
-		}
+	//    /// ------------------------------------------------------------------------------------
+	//    public void ViewDeactivated()
+	//    {
+	//    }
 
-		/// ------------------------------------------------------------------------------------
-		public bool IsOKToLeaveView(bool showMsgWhenNotOK)
-		{
-			return true;
-		}
+	//    /// ------------------------------------------------------------------------------------
+	//    public bool IsOKToLeaveView(bool showMsgWhenNotOK)
+	//    {
+	//        return true;
+	//    }
 
-		/// ------------------------------------------------------------------------------------
-		public System.Drawing.Image Image
-		{
-			get { return Resources.SendReceive; }
-		}
+	//    /// ------------------------------------------------------------------------------------
+	//    public System.Drawing.Image Image
+	//    {
+	//        get { return Resources.SendReceive; }
+	//    }
 
-		/// ------------------------------------------------------------------------------------
-		public ToolStripMenuItem MainMenuItem
-		{
-			get { return null; }
-		}
+	//    /// ------------------------------------------------------------------------------------
+	//    public ToolStripMenuItem MainMenuItem
+	//    {
+	//        get { return null; }
+	//    }
 
-		public string NameForUsageReporting
-		{
-			get { throw new NotImplementedException(); }
-		}
+	//    public string NameForUsageReporting
+	//    {
+	//        get { throw new NotImplementedException(); }
+	//    }
 
-		#endregion
-	}
+	//    #endregion
+	//}
 }

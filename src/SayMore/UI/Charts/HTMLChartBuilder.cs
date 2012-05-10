94c731c43a1a3821eac441cde0a96fc9dfe66c99
David Olson <domferrari@users.noreply.github.com>
2012-05-10 15:42:58 -0400

    Made the html DOCTYPE a public property in HTMLChartBuilder.cs

--- src/SayMore/UI/Charts/HTMLChartBuilder.cs
+++ src/SayMore/UI/Charts/HTMLChartBuilder.cs
@@ -238,11 +238,20 @@ namespace SayMore.Utilities.Charts
 
 		#region Methods for writing HTML tags
 		/// ------------------------------------------------------------------------------------
+		public static string XMLDocTypeInfo
+		{
+			get
+			{
+				return "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" " +
+					"\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">" +
+					"<html xml:lang=\"en\" lang=\"en\" xmlns=\"http://www.w3.org/1999/xhtml\">";
+			}
+		}
+
+		/// ------------------------------------------------------------------------------------
 		protected void OpenHtml()
 		{
-			_htmlText.Append("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" " +
-				"\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">" +
-			"<html xml:lang=\"en\" lang=\"en\" xmlns=\"http://www.w3.org/1999/xhtml\">");
+			_htmlText.Append(XMLDocTypeInfo);
 		}
 
 		/// ------------------------------------------------------------------------------------

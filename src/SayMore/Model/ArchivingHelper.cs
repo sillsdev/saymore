93990d4f15e2e707474cff8a5ce2259010e3b351
Phil Hopper <phillip_hopper@sil.org>
2013-12-23 16:24:30 -0500

    Allow user to choose a different IMDI program to launch.

--- src/SayMore/Model/ArchivingHelper.cs
+++ src/SayMore/Model/ArchivingHelper.cs
@@ -35,61 +35,17 @@ namespace SayMore.Model
 			var model = new IMDIArchivingDlgViewModel(Application.ProductName, element.Title, element.Id,
 				element.ArchiveInfoDetails, element is Project, element.SetFilesToArchive, destFolder)
 			{
-				HandleNonFatalError = (exception, s) => ErrorReport.NotifyUserOfProblem(exception, s),
-				PathToProgramToLaunch = Settings.Default.ProgramToLaunchForIMDIPackage = GetProgramToLaunchForIMDIPackage()
+				HandleNonFatalError = (exception, s) => ErrorReport.NotifyUserOfProblem(exception, s)
 			};
 
 			element.InitializeModel(model);
 
-			using (var dlg = new ArchivingDlg(model, ApplicationContainer.kSayMoreLocalizationId,
+			using (var dlg = new IMDIArchivingDlg(model, ApplicationContainer.kSayMoreLocalizationId,
 				Program.DialogFont, Settings.Default.ArchivingDialog))
 			{
 				dlg.ShowDialog();
 				Settings.Default.ArchivingDialog = dlg.FormSettings;
 			}
-		}
-
-		/// ------------------------------------------------------------------------------------
-		private static string GetProgramToLaunchForIMDIPackage()
-		{
-			string defaultProgram = Settings.Default.ProgramToLaunchForIMDIPackage;
-			if (!string.IsNullOrEmpty(defaultProgram))
-			{
-				if (File.Exists(defaultProgram))
-					return defaultProgram;
-
-				if (!Path.IsPathRooted(defaultProgram))
-				{
-					string rootedPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), defaultProgram);
-					if (File.Exists(rootedPath))
-						return rootedPath;
-					rootedPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), defaultProgram);
-					if (File.Exists(rootedPath))
-						return rootedPath;
-				}
-			}
-
-			using (var chooseIMDIProgram = new OpenFileDialog())
-			{
-				chooseIMDIProgram.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
-				chooseIMDIProgram.RestoreDirectory = true;
-				chooseIMDIProgram.CheckFileExists = true;
-				chooseIMDIProgram.CheckPathExists = true;
-				chooseIMDIProgram.Filter = string.Format("{0} ({1})|{1}|{2} ({3})|{3}",
-					LocalizationManager.GetString("DialogBoxes.ArchivingDlg.ProgramsFileTypeLabel", "Programs"),
-					"*.exe;*.pif;*.com;*.bat;*.cmd",
-					LocalizationManager.GetString("DialogBoxes.ArchivingDlg.AllFilesLabel", "All Files"),
-					"*.*");
-				chooseIMDIProgram.FilterIndex = 0;
-				chooseIMDIProgram.Multiselect = false;
-				chooseIMDIProgram.Title = LocalizationManager.GetString(
-					"DialogBoxes.ArchivingDlg.SelectIMDIProgram", "Select the program to launch after IMDI package is created");
-				chooseIMDIProgram.ValidateNames = true;
-				if (chooseIMDIProgram.ShowDialog() == DialogResult.OK && File.Exists(chooseIMDIProgram.FileName))
-					return chooseIMDIProgram.FileName;
-			}
-
-			return string.Empty;
 		}
 
 		/// ------------------------------------------------------------------------------------

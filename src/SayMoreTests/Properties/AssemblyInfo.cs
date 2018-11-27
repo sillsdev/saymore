5e1f2ba98424ca8c9ef25220009759c7f1b010df
John Thomson <john_thomson@sil.org>
2018-11-27 15:47:03 -0600

    Repair things so unit tests pass after recent SLDR change

--- src/SayMoreTests/Properties/AssemblyInfo.cs
+++ src/SayMoreTests/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using SIL.TestUtilities;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -34,3 +35,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("6.7.6.789")]
 [assembly: AssemblyVersion("6.7.6.789")]
 [assembly: AssemblyFileVersion("6.7.6.789")]
+
+// Somehow this makes it OK for tests not to call Sldr.Initialize().
+[assembly: OfflineSldr]

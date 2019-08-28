3db869e321ae2e253adf9850f259bc81d92dae1c
Phillip Hopper <phillip_hopper@sil.org>
2019-08-28 08:51:20 -0400

    SP-2114: Update data on the Project Progress tab

---
+++ src/SayMoreTests/ApplicationContainerTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using SayMore;
+using SayMore.Model.Files;
+using SayMore.Properties;
+
+namespace SayMoreTests
+{
+	[TestFixture]
+	public class ApplicationContainerTests
+	{
+		[Test]
+		public void ComponentRoles_ContainsOralTranslationRole_HasCorrectRenamingSuffix()
+		{
+			// get list of all available roles
+			var roles = ApplicationContainer.ComponentRoles.ToList();
+
+			// get the oral translation role
+			var oralTranslationRole = roles.FirstOrDefault(r => r.Id == ComponentRole.kOralTranslationComponentRoleId);
+			Assert.NotNull(oralTranslationRole);
+
+			// OralAnnotationTranslationSegmentFileSuffix must start with the oral translation role RenamingTemplateSuffix
+			var renamingSuffix = ComponentRole.kFileSuffixSeparator + oralTranslationRole.GetRenamingTemplateSuffix();
+			Assert.IsTrue(Settings.Default.OralAnnotationTranslationSegmentFileSuffix.StartsWith(renamingSuffix));
+		}
+	}
+}

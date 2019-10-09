b1f56bf2c84dfa4e18b81f413cac591459eece35
Phillip Hopper <phillip_hopper@sil.org>
2019-10-09 09:15:47 -0400

    Unit test changes

--- src/SayMoreTests/ApplicationContainerTests.cs
+++ src/SayMoreTests/ApplicationContainerTests.cs
@@ -8,18 +8,17 @@ namespace SayMoreTests
 	[TestFixture]
 	public class ApplicationContainerTests
 	{
-		[TestCase(ComponentRole.kOralTranslationComponentRoleId, "_Translation.wav", ExpectedResult = true)]
-		[TestCase(ComponentRole.kCarefulSpeechComponentRoleId, "_Careful.wav", ExpectedResult = true)]
-		public bool GetRenamingTemplateSuffix_RolesWithSegmentFileSuffix_SegmentFileSuffixStartsWithRenamingTemplateSuffix(string roleId, string segmentFileSuffix)
+		[TestCase(ComponentRole.kOralTranslationComponentRoleId, "_Translation.wav")]
+		[TestCase(ComponentRole.kCarefulSpeechComponentRoleId, "_Careful.wav")]
+		public void GetRenamingTemplateSuffix_SegmentFileRoleId_SegmentFileSuffixStartsWithRenamingTemplateSuffix(string roleId, string segmentFileSuffix)
 		{
 			// get the oral translation role
 			var role = ApplicationContainer.ComponentRoles.Single(r => r.Id == roleId);
-			Assert.NotNull(role);
 
 			// segment file suffix must start with the role RenamingTemplateSuffix
 			var renamingSuffix = ComponentRole.kFileSuffixSeparator + role.GetRenamingTemplateSuffix();
 
-			return segmentFileSuffix.StartsWith(renamingSuffix);
+			Assert.IsTrue(segmentFileSuffix.StartsWith(renamingSuffix));
 		}
 	}
 }

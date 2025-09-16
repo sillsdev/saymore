3eb0d8998bc41282f30aba3013ea0c6fbbaabb97
tombogle <tom_bogle@sil.org>
2025-09-16 11:21:53 -0400

    Made EditorBaseTests get ignored in non-UIInteractive environment since they can't really provide meaningful results

--- src/SayMoreTests/UI/ComponentEditors/EditorBaseTests.cs
+++ src/SayMoreTests/UI/ComponentEditors/EditorBaseTests.cs
@@ -32,7 +32,7 @@ namespace SayMoreTests.UI.ComponentEditors
 
 			protected override void OnEditorAndChildrenLostFocus()
 			{
-				EditorAndChildrenLostFocus(this, null);
+				EditorAndChildrenLostFocus?.Invoke(this, null);
 			}
 
 			protected override void OnFormLostFocus()
@@ -56,6 +56,9 @@ namespace SayMoreTests.UI.ComponentEditors
 		[SetUp]
 		public void Setup()
 		{
+			if (!Environment.UserInteractive)
+				Assert.Ignore("Ignored in CI");
+			
 			_editor = new TestEditor();
 			_testForm = new TestForm();
 			_testForm.Controls.Add(_editor);
@@ -72,27 +75,22 @@ namespace SayMoreTests.UI.ComponentEditors
 			_panelInEditorInner.Controls.AddRange(new Control[] { _labelInEditor, _buttonInEditor });
 			_panelInEditorOuter.Controls.AddRange(new Control[] { _panelInEditorInner, _textBoxInEditor });
 			_editor.Controls.Add(_panelInEditorOuter);
+
+			Assert.That(_editor.ChildControls, Is.EquivalentTo(new Control[]
+			{
+				_panelInEditorOuter,
+				_panelInEditorInner,
+				_labelInEditor,
+				_buttonInEditor,
+				_textBoxInEditor
+			}));
 		}
 
 		/// ------------------------------------------------------------------------------------
 		[TearDown]
 		public void TearDown()
 		{
-			_testForm.Dispose();
-		}
-
-		/// ------------------------------------------------------------------------------------
-		[Test]
-		public void ChildControls_VerifyAllControlsInList()
-		{
-			Assert.That(_editor.ChildControls, Is.EquivalentTo(new Control[] 
-				{
-					_panelInEditorOuter,
-					_panelInEditorInner,
-					_labelInEditor, 
-					_buttonInEditor,
-					_textBoxInEditor
-				}));
+			_testForm?.Dispose();
 		}
 
 		/// ------------------------------------------------------------------------------------
@@ -121,12 +119,8 @@ namespace SayMoreTests.UI.ComponentEditors
 
 		/// ------------------------------------------------------------------------------------
 		[Test]
-		[Category("SkipOnTeamCity")]
 		public void OnFormLostFocus_FocusMovedOutsideForm_MethodCalled()
 		{
-			if (!Environment.UserInteractive)
-				Assert.Ignore("Ignored in CI");
-			
 			var otherForm = new TestForm();
 			_testForm.Activate();
 
